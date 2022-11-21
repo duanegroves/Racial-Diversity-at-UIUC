966995f0a09a92359162788886d3548215895162
duanegroves <samsuri2001@gmail.com>
2022-11-21 13:34:55 -0600

    Changed React to just be normal JS and put a basic barchart in

---
+++ client/src/components/AxisBottom.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AxisBottom = ({ xScale, innerHeight, tickFormat }) => {
+  return xScale.ticks().map(tickValue => (
+    <g className="tick" key={tickValue} transform={`translate(${xScale(tickValue)},0)`}>
+      <line y2={innerHeight} />
+      <text style={{ textAnchor: 'middle' }} dy=".71em" y={innerHeight + 3}>
+        {tickFormat(tickValue)}
+      </text>
+    </g>
+  ));
+}
+
+export default AxisBottom;
---
+++ client/src/components/AxisLeft.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AxisLeft = ({ yScale }) => {
+  return yScale.domain().map(tickValue => (
+    <g className="tick">
+      <text
+        key={tickValue}
+        style={{ textAnchor: 'end' }}
+        x={-3}
+        dy=".32em"
+        y={yScale(tickValue) + yScale.bandwidth() / 2}
+      >
+        {tickValue}
+      </text>
+    </g>
+  ));
+}
+
+export default AxisLeft;
+  
