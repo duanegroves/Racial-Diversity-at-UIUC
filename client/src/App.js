eeb2f1d0cf30608299e36d6c61e3231ad5cf81fb
duanegroves <samsuri2001@gmail.com>
2022-11-21 16:59:11 -0600

    Created d3 readable csv's. Realized d3 requires a certain format for its data and my old way does not work

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { csv, arc, pie, scaleBand, scaleLinear, max, format } from 'd3';
+import { scaleBand, scaleLinear, max, format } from 'd3';
 import './App.css';
 import useData from './components/useData';
 import AxisBottom from './components/AxisBottom';
