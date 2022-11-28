47be595864313dbb516f423a9232bf2dfaaa9343
duanegroves <samsuri2001@gmail.com>
2022-11-28 16:40:12 -0600

    Major Aggregation not also calculates Major totals without grouping on degree

--- data/csv_to_json_v2.py
+++ data/csv_to_json_v2.py
@@ -8,6 +8,7 @@ def make_json(csvFilePath, jsonFilePath):
    
    with open(csvFilePath, encoding='utf-8-sig') as csv_file:
       csvreader = csv.reader(csv_file)
+
       for idx, row in enumerate(csvreader, start=1):
          row = [el.strip() for el in row]
 
@@ -23,12 +24,18 @@ def make_json(csvFilePath, jsonFilePath):
             # Check to see if major and degree already exists 
             # If it does we just add the values
             # Otherwise we create a new row
-            foundExistingMajor = False
+            foundExistingMajorTotal = False
+            foundExistingMajorDegree = False
             for saved_idx, saved_row in enumerate(data):
                if saved_row[3] == row[3] and saved_row[4] == row[4]:
-                  foundExistingMajor = True
-                  data[saved_idx] = [int(row[i]) + int(saved_row[i]) if 7 < i and i < 28 else row[i] for i, el in enumerate(row)]
-            if not foundExistingMajor:
+                  foundExistingMajorDegree = True
+                  data[saved_idx] = [int(row[i]) + int(saved_row[i]) if 7 < i and i < 28 else saved_row[i] for i, el in enumerate(row)]
+               if saved_row[3] == "***Major total***" and saved_row[4] == row[4]:
+                  foundExistingMajorTotal = True
+                  data[saved_idx] = [int(row[i]) + int(saved_row[i]) if 7 < i and i < 28 else saved_row[i] for i, el in enumerate(row)]
+            if not foundExistingMajorTotal:
+               data.append(["***Major total***" if i == 3 else el for i, el in enumerate(row)])
+            if not foundExistingMajorDegree:
                data.append(row)
 
          
